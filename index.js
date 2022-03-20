56b9e32b7fe4c4e23e3a634ef40575e217eae30b
Justus-Onyancha <justus.onyancha@moringaschool.com>
2022-03-20 17:23:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -11,16 +11,23 @@ const returnLastTwoDrivers = function(drivers){
 }
 
 
+//returns firstTwoDrivers ans lastTwoDrivers as its elements in an array
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 
+
+// should multiply a given value using created muiliplier
 const createFareMultiplier = function(fareMultiplier){
     return function(value){
         return fareMultiplier * value
     }
 }
 
+// faredubler fucntion from createmulpiplier fucntion above
 const fareDoubler = createFareMultiplier(2)
+
+// fareTripler fucntion from createmulpiplier fucntion above
 const fareTripler = createFareMultiplier(3)
+
 
 const selectDifferentDrivers = function(drivers, driverToReturn){
     return driverToReturn(drivers)
